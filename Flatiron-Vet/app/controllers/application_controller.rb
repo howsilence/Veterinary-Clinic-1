c8b475d3b1772fbe7d491866cae2b1d3fe4e44a7
nathalia <nathaliatroina@gmail.com>
2022-03-23 16:06:52 -0400

    error handling and app controller

--- Flatiron-Vet/app/controllers/application_controller.rb
+++ Flatiron-Vet/app/controllers/application_controller.rb
@@ -1,8 +1,23 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  def hello_world
-    session[:count] = (session[:count] || 0) + 1
-    render json: { count: session[:count] }
-  end
+
+private
+
+def render_unprocessable_entity_response(invalid)
+  render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+end
+
+
+def render_not_found(error)
+  render json: { error: "#{model.errors} not found" } status: :not_found
+end
+
+
+#   def hello_world
+#     session[:count] = (session[:count] || 0) + 1
+#     render json: { count: session[:count] }
+#   end
 end
