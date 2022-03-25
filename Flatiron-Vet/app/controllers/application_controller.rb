15988ee0ab1d80eb8bc2d602b907932b7a570b34
howsilence <statton0104@gmail.com>
2022-03-25 14:47:20 -0400

    setup user controller for password auth

--- Flatiron-Vet/app/controllers/application_controller.rb
+++ Flatiron-Vet/app/controllers/application_controller.rb
@@ -1,23 +1,33 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  # before_action :authorize
+  # skip_before_action :hello_world
+
+  def hello_world
+    session[:count] = (session[:count] || 0) + 1
+    render json: { count: session[:count] }
+  end
+
+  private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
 
 
-private
+  def render_not_found(error)
+    render json: { error: "#{model.errors} not found" }, status: :not_found
+  end
 
-def render_unprocessable_entity_response(invalid)
-  render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
-end
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
 
 
-def render_not_found(error)
-  render json: { error: "#{model.errors} not found" } status: :not_found
-end
-
-
-#   def hello_world
-#     session[:count] = (session[:count] || 0) + 1
-#     render json: { count: session[:count] }
-#   end
 end
--- Flatiron-Vet/app/models/user.rb
+++ Flatiron-Vet/app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
 
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :username, presence: true, uniqueness: true  
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    
 
     has_secure_password
 end
