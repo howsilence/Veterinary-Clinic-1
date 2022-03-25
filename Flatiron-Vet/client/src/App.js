84cb37991171683d97c5312311b015c3c24c3e39
howsilence <statton0104@gmail.com>
2022-03-25 16:07:01 -0400

    started frontend for user

--- Flatiron-Vet/client/src/App.js
+++ Flatiron-Vet/client/src/App.js
@@ -3,6 +3,18 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [user, setUser] = useState([]);
+
+    useEffect(() => {
+      fetch("http://localhost:4000/users/1")
+        .then((r) => r.json())
+        .then((userObj) => {
+          console.log(userObj)
+          setUser(userObj);
+        });
+    }, []);
+
+    console.log(user)
 
   useEffect(() => {
     fetch("/hello")
@@ -19,6 +31,12 @@ function App() {
           </Route>
           <Route path="/">
             <h1>Page Count: {count}</h1>
+          </Route>
+          <Route path="/createuser">
+            <createUserForm /> //build out new user form here
+          </Route>
+          <Route path="/user">
+            <UserInfo /> // build out user information page, allow CRUD
           </Route>
         </Switch>
       </div>
--- Flatiron-Vet/config/routes.rb
+++ Flatiron-Vet/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :create, :destroy, :update]
   resources :sessions, only: [:create, :destroy]
 
-
+  # get '/users/:id', to: 'users#show'
   get "/hello", to: "application#hello_world"
 
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
