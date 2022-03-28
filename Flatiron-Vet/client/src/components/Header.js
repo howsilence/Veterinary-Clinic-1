d34f5ad9eee66c507463c9d353f8f269d1843404
howsilence <statton0104@gmail.com>
2022-03-28 10:39:40 -0400

    working header

--- Flatiron-Vet/client/src/components/Header.js
+++ Flatiron-Vet/client/src/components/Header.js
@@ -1,30 +1,33 @@
+import React from 'react'
+
 function Header() {
 
 
 
 
     return(
+
         <header class="header colored sticky-header" data-scroll="out">
 				<div class="c-container">
 					<div class="nav-items">
-					<a href="">
+					<a href="/">
 					<img class="site-logo" src="https://md-aqil.github.io/images/logo2.png" alt=""/>
 					</a>
 					<ul class="main-nav">
 						<li class="nav-item active">
-						<a class="nav-link active" href="#">Home</a>
+						<a class="nav-link active" href="/">Home</a>
 						</li>
 						<li class="nav-item">
-						<a class="nav-link" href="#">Our Team</a>
+						<a class="nav-link" href="/team">Our Team</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#">F.A.Q.</a>
+							<a class="nav-link" href="/faq">F.A.Q.</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="#">Latest News</a>
+								<a class="nav-link" href="/news">Latest News</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="#">Testimonials</a>
+								<a class="nav-link" href="/testimonials">Testimonials</a>
 							</li>
 					</ul>
 						<div class="form-inline">
@@ -35,11 +38,6 @@ function Header() {
 					</div>
 				</div>
 			</header>
-
-
-
-
-
 
     )
 }
